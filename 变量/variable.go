badbfb528f486b95b6abb2a1f0db1bcf811f1ff3
oscar-mx <oscarmx0912@outlook.com>
2019-12-16 21:59:59 +0800

    variable

--- 变量/variable.go
+++ 变量/variable.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+var c int //全局变量
+var a float32 = 3.14
 func main() {
 	fmt.Println("变量demo")
 /*
@@ -36,7 +38,7 @@ func main() {
 	fmt.Println(a,b,c)
 
  */
-
+/*
 	a, b := 10, 20
 
 	//可以通过多重赋值进行交换
@@ -44,4 +46,22 @@ func main() {
 
 	fmt.Println(a)
 	fmt.Println(b)
+*/
+/*
+	var a int = 1
+	var b int = 2
+	//声明局部变量 c 并计算 a 和 b 的和
+	//%d是一个占位符 表示输出十进制整数
+	c := a + b
+	fmt.Printf("a = %d, b = %d, c = %d\n", a, b, c)// 输出a = 1, b = 2, c = 3
+*/
+	c = 10
+	var a int = 3
+	fmt.Printf("c = %d\n",c)//输出 c=10
+	fmt.Printf("a = %d\n", a)//输出 a=3
+	add(1,2)//输出3
+}
+func add(s1 int, s2 int) {
+	sum := s1 + s2
+	fmt.Println(sum)
 }
