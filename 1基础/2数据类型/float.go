54165f7b3b39c6a5109f5740be51e9d24ce779c5
mx <oscarmx0912@outlook.com>
2019-12-24 20:50:44 +0800

    Create float.go

---
+++ 1基础/2数据类型/float.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+const e = .71828 // 0.71828
+const f = 1.     // 1
+
+func main0101() {
+
+    //浮点型数据 分为 单精度浮点型 float32（小数位数为7位） 双精度浮点型 float64（小数位数为15位）
+    //float64 比 float32 更精准
+    //var a float64=1223.456
+    ////保留六位小数数据会更精准
+    //fmt.Printf("%f\n",a)
+    //var a float32 =3.14
+    //
+    //fmt.Printf("%.2f",a)
+
+    //通过自动推到类型创建的浮点型变量 默认类型为float64
+    a := 123.456
+    fmt.Printf("%T\n", a)
+}
